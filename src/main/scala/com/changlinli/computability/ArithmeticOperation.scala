f268e8bb954bda919ac93a92abb319ea58a0a8b5
Changlin Li <mail@changlinli.com>
2018-09-22 17:13:00 -0400

    Add nesting for parsing arithmetic operation
    Support operator precedence and parentheses

--- src/main/scala/com/changlinli/computability/ArithmeticOperation.scala
+++ src/main/scala/com/changlinli/computability/ArithmeticOperation.scala
@@ -16,6 +16,7 @@ object ArithmeticOperation {
       .map(NaturalNum.fromInt)
       .collect{case Some(x) => x}
       .map(NumLiteral.apply)
+      .flatMap(x => many(horizontalWhitespace) *> x.pure[Parser])
 
   def parsePlus: Parser[Plus] = for {
     x <- parseNumLiteral
@@ -33,23 +34,52 @@ object ArithmeticOperation {
     y <- parseNumLiteral
   } yield Multiply(x, y)
 
+  private def chainLeft1[A](elemParser: Parser[A], operatorParser: => Parser[(A, A) => A]): Parser[A] = {
+    def rest(z: A): Parser[A] =
+      (for {
+        operator <- operatorParser
+        elem <- elemParser
+        result <- rest(operator(z, elem))
+      } yield result) | z.pure[Parser]
+    elemParser.flatMap(rest)
+  }
+
+  def additionMultiTerm: Parser[ArithmeticOperation] = chainLeft1(multiTerm, parseAdd)
+
+  def multiTerm: Parser[ArithmeticOperation] = chainLeft1(parseAtomic.covary[ArithmeticOperation], parseMulti)
+
+  def parseAtomic: Parser[ArithmeticOperation] = myParens(additionMultiTerm) | parseNumLiteral.covary[ArithmeticOperation]
+
+  def myParens[A](p: => Parser[A]): Parser[A] =
+    bracket(char('('), p, char(')')) <~ many(horizontalWhitespace)
+
+  def parseAdd: Parser[(ArithmeticOperation, ArithmeticOperation) => ArithmeticOperation] =
+    for {
+      _ <- char('+')
+      _ <- many(horizontalWhitespace)
+    } yield Plus(_, _)
+
+  def parseMulti: Parser[(ArithmeticOperation, ArithmeticOperation) => ArithmeticOperation] =
+    for {
+      _ <- char('*')
+      _ <- many(horizontalWhitespace)
+    } yield Multiply(_, _)
+
   def calculateThroughLambdaTerm(x: ArithmeticOperation): NonEmptyList[LambdaTerm] =
     LambdaTerm.fromArithmeticOperationWithAutomaticNames(x)
       .|>(LambdaTerm.betaReduceScan)
 
-  def parseArithmeticOperation: Parser[ArithmeticOperation] =
-    parsePlus.covary[ArithmeticOperation]
-      .|(parseMultiply.covary[ArithmeticOperation])
-      .|(parseNumLiteral.covary[ArithmeticOperation])
-
   def fromString(str: String): Option[ArithmeticOperation] = {
-    parseArithmeticOperation.parseOnly(str) match {
+    additionMultiTerm.parseOnly(str) match {
       case ParseResult.Done(_, result) =>
         Some(result)
       case ParseResult.Partial(_) =>
-        throw new Exception("This should be impossible because we parseOnly-ed insead of parse-ing")
+        throw new Exception("This should be impossible because we parseOnly-ed instead of parse-ing")
       case ParseResult.Fail(_, _, _) =>
         None
     }
   }
+
+  def calculateFromString(str: String): Option[NonEmptyList[LambdaTerm]] =
+    fromString(str).map(calculateThroughLambdaTerm)
 }
--- src/test/scala/com/changlinli/computability/TestArithmeticOperation.scala
+++ src/test/scala/com/changlinli/computability/TestArithmeticOperation.scala
@@ -7,6 +7,24 @@ class TestArithmeticOperation extends FlatSpec {
     val one = NumLiteral(NaturalNum.fromInt(1).get)
     assert(ArithmeticOperation.fromString("1 + 1") === Some(Plus(one, one)))
   }
+  it should "parse '1 + 2 * 3' with the correct operator precedence" in {
+    val one = NumLiteral(NaturalNum.fromInt(1).get)
+    val two = NumLiteral(NaturalNum.fromInt(2).get)
+    val three = NumLiteral(NaturalNum.fromInt(3).get)
+    assert(ArithmeticOperation.fromString("1 + 2 * 3") === Some(Plus(one, Multiply(two, three))))
+  }
+  it should "parse '1 * 2 + 3' with the correct operator precedence" in {
+    val one = NumLiteral(NaturalNum.fromInt(1).get)
+    val two = NumLiteral(NaturalNum.fromInt(2).get)
+    val three = NumLiteral(NaturalNum.fromInt(3).get)
+    assert(ArithmeticOperation.fromString("1 * 2 + 3") === Some(Plus(Multiply(one, two), three)))
+  }
+  it should "parse '(1 + 2) * 3' with the correct operator precedence" in {
+    val one = NumLiteral(NaturalNum.fromInt(1).get)
+    val two = NumLiteral(NaturalNum.fromInt(2).get)
+    val three = NumLiteral(NaturalNum.fromInt(3).get)
+    assert(ArithmeticOperation.fromString("(1 + 2) * 3") === Some(Multiply(Plus(one, two), three)))
+  }
 
   "The ArithmeticOperation representing 1 + 1" should "calculate as 2 via the lambda calculus" in {
     val one = NumLiteral(NaturalNum.fromInt(1).get)
@@ -23,5 +41,11 @@ class TestArithmeticOperation extends FlatSpec {
       .last
       .|>(LambdaTerm.convertToNaturalNum)
     assert(calculationResult === Some(Succ(Succ(Succ(Succ(Succ(Succ(Zero))))))))
+  }
+
+  "calculateFromString" should "calculate (1 + 2) * 3 as 9" in {
+    val nineOpt = NaturalNum.fromInt(9)
+    println(ArithmeticOperation.calculateFromString("(1 + 2) * 3").toList.flatMap(_.toList).map(LambdaTerm.prettyPrint).mkString("\n"))
+    assert(ArithmeticOperation.calculateFromString("(1 + 2) * 3").flatMap(_.last.|>(LambdaTerm.convertToNaturalNum)) === nineOpt)
   }
 }
